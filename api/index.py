f19adf5c499585a49f6af8f25b1bc30fb9ccd5d5
Second Farand <randomoke123@gmail.com>
2026-05-12 23:35:50 +0800

    feat: add three filter and recommendation endpoint

--- api/index.py
+++ api/index.py
@@ -48,7 +48,7 @@ def get_engine():
 #         DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+pg8000://", 1)
 
 # engine = create_engine(DATABASE_URL)
-
+# 1. GET RECIPE
 @app.get("/api/recipe")
 def get_resep(limit: int = 10):
     try:
@@ -65,6 +65,67 @@ def get_resep(limit: int = 10):
         # }
     except Exception as e:
         return {"error": str(e)}
+
+# 2. GET by INGREDIENTS-LIST
+@app.get("/api/recipe/cari-bahan")
+def cari_bahan(bahan: str, limit: int = 10):
+    try:
+        if not bahan:
+            return {"status": "success", "message": "Silahkan masukkan nama bahan", "results": []}
+        
+        list_bahan = [b.strip() for b in bahan.split(",") if b.strip()]
+        conditions = []
+        for b in list_bahan:
+            conditions.append(f"\"ingredients_list\" ILIKE '%{b}%'")
+        
+        where_clause = " WHERE " + " AND ".join(conditions)
+        query = f'SELECT * FROM "tb_recipe" {where_clause} LIMIT {limit}'
+
+        df = pd.read_sql(query, get_engine())
+        df.columns = df.columns.str.strip() 
+        
+        return {
+            "status": "success",
+            "search_query": bahan,
+            "total_found": len(df),
+            "results": df.to_dict(orient="records")
+        }
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+# 3. GET by CATEGORY and EST. TIME
+@app.get("/api/recipe/filter")
+def filter_resep(category: str = None, waktu_max: int = None, limit: int = 10):
+    try:
+        conditions = []
+        if category:
+            conditions.append(f"title ILIKE '%{category}%'")
+        if waktu_max:
+            conditions.append(f"estimated_time <= {waktu_max}")
+        
+        where_clause = " WHERE " + " AND ".join(conditions) if conditions else ""
+        query = f'SELECT * FROM "tb_recipe" {where_clause} LIMIT {limit}'
+        
+        df = pd.read_sql(query, get_engine())
+        df.columns = df.columns.str.strip()
+        
+        return {"status": "success", "results": df.to_dict(orient="records")}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    
+# 4. GET by LIKES for RECOMMENDATION
+@app.get("/api/recipe/recommendation")
+def get_rekomendasi():
+    try:
+        query = 'SELECT * FROM "tb_recipe" ORDER BY loves DESC LIMIT 50'
+        df = pd.read_sql(query, get_engine())
+        df.columns = df.columns.str.strip()
+        
+        rekomendasi = df.sample(5).to_dict(orient="records")
+        
+        return {"status": "success", "results": rekomendasi}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
 
 @app.get("/api/health")
 def health_check():
